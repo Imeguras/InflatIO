843fbf1646e2671d38e0e9ba8aea160fa0ec8a29
iogrt <duarte.dd.dias@gmail.com>
2022-02-13 21:28:51 +0000

    turn into a library

--- product_crawler.py
+++ product_crawler.py
@@ -3,21 +3,28 @@ from selenium.webdriver.common.by import By
 import api
 from datetime import datetime
 from sys import stderr
+import traceback
 
+
+<<<<<<< Updated upstream
 driver = webdriver.Chrome()
+=======
+>>>>>>> Stashed changes
 
 def getTimestamp():
   return datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
 
 f = open("errors.txt",'a')
-def logError(msg):
-    s = getTimestamp() + msg
+file2 = open("quantities.txt",'a')
+def logError(msg,error):
+    s = getTimestamp() + msg + str(error)
     print(s, file=stderr)
     print(s, file=f)
+    traceback.print_exc()
+    traceback.print_exc(file=f)
 
-def continente_urls():
+def continente_urls(driver):
     driver.get("https://continente.pt");
-
     # ignores "Ver Todos" & broader category links
     linkNodes = driver.find_elements(By.CSS_SELECTOR, "a:not(.dropdown-toggle):not(.ct-font--opensans-italic).dropdown-link")
     urls = []
@@ -30,8 +37,12 @@ def continente_urls():
     return urls
 
 
+<<<<<<< Updated upstream
 def crawl (driverUrl):
     db = api.db_init()
+=======
+def crawl (driver, driverUrl):
+>>>>>>> Stashed changes
     # continente specific method
     getUrl = lambda page:driverUrl+'?start='+str(page);
     #extracts the category from the url, this could be done up there but it would require an array or memory to be passed on
@@ -65,44 +76,54 @@ def crawl (driverUrl):
                 #Url has been "historically" fetched as a hyperlink from clicking the title
                 measureKey = quantityElement.text[-2:]
                 
-                # In the website the value can be "emb. 50 gr" OR "emb.50 gr"
-                # see https://www.continente.pt/produto/batata-frita-lisa-yorkeso-ruffles-7390758.html for a bad input
-                # can also be "emb. x gr (peso escorrido x )" 
-                rawQuantity = float(quantityElement.text.split(".")[1].split(" ")[1].replace(',','.'))
-                measures={
-                    "lt":(1,'l'),
-                    " L":(1,'l'),
-                    "dl":(10,'l'),
-                    "cl":(100,'l'),
-                    "ml":(1000,'l'),
-                    "kg":(1,'kg'),
-                    "gr":(1000,'kg'),
-                    "un":(1,'un')
-                }
-                data = {
-                    "url": nameElement.get_attribute('href'),
-                    "name": nameElement.text,
-                    "price": priceElement.text[1:].replace(",","."),
-                    #For this website "Desconto Imediato: " precedes the actual discount and it comes in percentage not the decimal value for analitical purposes
-                    "discount": 0 if len(discountElements)==0 else float(discountElements[0].text[19:-1])/100,
-                    #TODO multithreading theres no way the crawler gonna do a switch/if statement for each element when most of the time its gonna be measure in the right measurment
-                    "measure": measures[measureKey][1],
-                    "quantity": rawQuantity/measures[measureKey][0],
-                    "brand": brandElement.text,
-                    "category": categoryElement
-                }
-                elements.append(data)
-                print('collected: '+ str(data))
+                # # In the website the value can be "emb. 50 gr" OR "emb.50 gr"
+                # # see https://www.continente.pt/produto/batata-frita-lisa-yorkeso-ruffles-7390758.html for a bad input
+                # # can also be "emb. x gr (peso escorrido x )" 
+                # rawQuantity = float(quantityElement.text.split(".")[1].split(" ")[1].replace(',','.'))
+                # measures={
+                #     "lt":(1,'l'),
+                #     " L":(1,'l'),
+                #     "dl":(10,'l'),
+                #     "cl":(100,'l'),
+                #     "ml":(1000,'l'),
+                #     "kg":(1,'kg'),
+                #     "gr":(1000,'kg'),
+                #     "un":(1,'un')
+                # }
+                # data = {
+                #     "url": nameElement.get_attribute('href'),
+                #     "name": nameElement.text,
+                #     "price": priceElement.text[1:].replace(",","."),
+                #     #For this website "Desconto Imediato: " precedes the actual discount and it comes in percentage not the decimal value for analitical purposes
+                #     "discount": 0 if len(discountElements)==0 else float(discountElements[0].text[19:-1])/100,
+                #     #TODO multithreading theres no way the crawler gonna do a switch/if statement for each element when most of the time its gonna be measure in the right measurment
+                #     "measure": measures[measureKey][1],
+                #     "quantity": rawQuantity/measures[measureKey][0],
+                #     "brand": brandElement.text,
+                #     "category": categoryElement
+                # }
+                # elements.append(data)
+                # print('collected: '+ str(data))
             except Exception as e:
                 logError("tile error, WIP state")
     api.db_ins_entry(db, elements)
 
-def main():
-    urls = continente_urls()
+def start():
+  return {
+    "driver" : webdriver.Chrome(executable_path="./driver"),
+    "dbconn": api.db_init()
+  }
+def crawlUrls(driver, filename="./crawlCache/continenteUrls"):
+    urls = continente_urls(driver)
     print("urls fetched")
-    for url in urls:
-        crawl(url)
+    with open(filename,'w') as f:
+          print("\n".join(urls),file=f)
 
+def crawlCachedUrls(driver):
+    with open('crawlCache/continenteUrls') as f:
+        urls = f.readlines()
+        for url in urls:
+            crawl(driver,url)
 
-main()
-driver.close()
+#main()
+#driver.close()
